dc57aa82f2a201b6e125050815d73fe0aa58215d
owen <xiaotu.dev@gmail.com>
2016-10-09 22:03:27 +0800

    use ViewStub to lazy load layout

--- statuslayout/src/main/java/com/owen/statuslayout/StatusLayout.java
+++ statuslayout/src/main/java/com/owen/statuslayout/StatusLayout.java
@@ -8,152 +8,225 @@ import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewStub;
 import android.widget.FrameLayout;
 
 public class StatusLayout extends FrameLayout {
 
-	private Context mContext;
+    private Context mContext;
 
-	private FrameLayout mContentViewContainer;
-	private FrameLayout mProgressLayoutContainer;
-	private FrameLayout mEmptyLayoutContainer;
-	private FrameLayout mErrorLayoutContainer;
+    private FrameLayout mContentViewContainer;
+    private FrameLayout mProgressLayoutContainer;
+    private FrameLayout mEmptyLayoutContainer;
+    private FrameLayout mErrorLayoutContainer;
 
-	public enum MODE {
-		PROGRESS,
-		EMPTY,
-		CONTENT,
-		ERROR
-	}
+    private int mProgressLayout;
+    private int mEmptyLayout;
+    private int mErrorLayout;
 
-	public StatusLayout(Context context) {
-		super(context);
-		init(context, null, 0);
-	}
+    public enum MODE {
+        PROGRESS,
+        EMPTY,
+        CONTENT,
+        ERROR
+    }
 
-	public StatusLayout(Context context, AttributeSet attrs) {
-		super(context, attrs);
-		init(context, attrs, 0);
-	}
+    public StatusLayout(Context context) {
+        super(context);
+        init(context, null, 0);
+    }
 
-	public StatusLayout(Context context, AttributeSet attrs, int defStyleAttr) {
-		super(context, attrs, defStyleAttr);
-		init(context, attrs, defStyleAttr);
-	}
+    public StatusLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context, attrs, 0);
+    }
 
-	@TargetApi(Build.VERSION_CODES.LOLLIPOP)
-	public StatusLayout(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
-		super(context, attrs, defStyleAttr, defStyleRes);
-		init(context, attrs, defStyleAttr);
-	}
+    public StatusLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context, attrs, defStyleAttr);
+    }
 
-	private void init(Context context, AttributeSet attrs, int defStyle) {
-		mContext = context;
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public StatusLayout(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init(context, attrs, defStyleAttr);
+    }
 
-		TypedArray typedArray = mContext.obtainStyledAttributes(attrs, R.styleable.StatusLayout);
-		try {
-			int progressLayout = typedArray.getResourceId(R.styleable.StatusLayout_progressLayout,
-					R.layout.status_layout_progress);
-			int emptyLayout = typedArray.getResourceId(R.styleable.StatusLayout_emptyLayout,
-					R.layout.status_layout_empty);
-			int errorLayout = typedArray.getResourceId(R.styleable.StatusLayout_errorLayout,
-					R.layout.status_layout_error);
+    private void init(Context context, AttributeSet attrs, int defStyle) {
+        mContext = context;
 
-			initLayoutContainer();
-			addViewToContainer(progressLayout, emptyLayout, errorLayout);
-		} finally {
-			typedArray.recycle();
-		}
-	}
+        TypedArray typedArray = mContext.obtainStyledAttributes(attrs, R.styleable.StatusLayout);
+        try {
+            mProgressLayout = typedArray.getResourceId(R.styleable.StatusLayout_progressLayout,
+                    R.layout.status_layout_progress);
+            mEmptyLayout = typedArray.getResourceId(R.styleable.StatusLayout_emptyLayout,
+                    R.layout.status_layout_empty);
+            mErrorLayout = typedArray.getResourceId(R.styleable.StatusLayout_errorLayout,
+                    R.layout.status_layout_error);
+        } finally {
+            typedArray.recycle();
+        }
 
-	private void initLayoutContainer() {
-		LayoutInflater.from(mContext).inflate(R.layout.status_layout, this);
-		mContentViewContainer = (FrameLayout) findViewById(R.id.status_content);
-		mProgressLayoutContainer = (FrameLayout) findViewById(R.id.status_progress);
-		mEmptyLayoutContainer = (FrameLayout) findViewById(R.id.status_empty);
-		mErrorLayoutContainer = (FrameLayout) findViewById(R.id.status_error);
-	}
+        LayoutInflater.from(mContext).inflate(R.layout.status_layout, this);
+        mContentViewContainer = (FrameLayout) findViewById(R.id.status_content);
+    }
 
-	private void addViewToContainer(int progressLayout, int emptyLayout, int errorLayout) {
-		LayoutInflater.from(mContext).inflate(progressLayout, mProgressLayoutContainer);
-		LayoutInflater.from(mContext).inflate(emptyLayout, mEmptyLayoutContainer);
-		LayoutInflater.from(mContext).inflate(errorLayout, mErrorLayoutContainer);
-	}
+    @Override
+    public void addView(View child, int index, ViewGroup.LayoutParams params) {
+        if (mContentViewContainer == null) {
+            super.addView(child, index, params);
+            return;
+        }
 
-	/**
-	 * StatusLayout have four sub layout: <br/>
-	 * 1) progress layout <br/> 
-	 * 2) content layout <br/>
-	 * 3) empty layout <br/>
-	 * 4) error layout <br/>
-	 * 
-	 * override this method to add all the views into content layout.
-	 */
-	@Override
-	public void addView(View child, int index, ViewGroup.LayoutParams params) {
-		if (mContentViewContainer == null) {
-			super.addView(child, index, params);
-		} else {
-			mContentViewContainer.addView(child, index, params);
-		}
-	}
+        if (mContentViewContainer.getChildCount() != 0) {
+            super.addView(child, index, params);
+        } else {
+            if (mContentViewContainer == null) {
+                super.addView(child, index, params);
+            } else {
+                mContentViewContainer.addView(child, index, params);
+            }
+        }
+    }
 
-	public void setMode(MODE mode) {
-		switch (mode) {
-		case PROGRESS:
-			mProgressLayoutContainer.setVisibility(View.VISIBLE);
-			mContentViewContainer.setVisibility(View.GONE);
-			mEmptyLayoutContainer.setVisibility(View.GONE);
-			mErrorLayoutContainer.setVisibility(View.GONE);
-			break;
-		case EMPTY:
-			mProgressLayoutContainer.setVisibility(View.GONE);
-			mEmptyLayoutContainer.setVisibility(View.VISIBLE);
-			mContentViewContainer.setVisibility(View.GONE);
-			mErrorLayoutContainer.setVisibility(View.GONE);
-			break;
-		case CONTENT:
-			mProgressLayoutContainer.setVisibility(View.GONE);
-			mEmptyLayoutContainer.setVisibility(View.GONE);
-			mContentViewContainer.setVisibility(View.VISIBLE);
-			mErrorLayoutContainer.setVisibility(View.GONE);
-			break;
-		case ERROR:
-			mProgressLayoutContainer.setVisibility(View.GONE);
-			mEmptyLayoutContainer.setVisibility(View.GONE);
-			mContentViewContainer.setVisibility(View.GONE);
-			mErrorLayoutContainer.setVisibility(View.VISIBLE);
-			break;
-		}
-	}
+    public void setMode(MODE mode) {
+        switch (mode) {
+            case PROGRESS:
+                setProgressLayoutVisibility(true);
+                setEmptyLayoutVisibility(false);
+                setContentLayoutVisibility(false);
+                setErrorLayoutVisibility(false);
+                break;
+            case EMPTY:
+                setProgressLayoutVisibility(false);
+                setEmptyLayoutVisibility(true);
+                setContentLayoutVisibility(false);
+                setErrorLayoutVisibility(false);
+                break;
+            case CONTENT:
+                setProgressLayoutVisibility(false);
+                setEmptyLayoutVisibility(false);
+                setContentLayoutVisibility(true);
+                setErrorLayoutVisibility(false);
+                break;
+            case ERROR:
+                setProgressLayoutVisibility(false);
+                setEmptyLayoutVisibility(false);
+                setContentLayoutVisibility(false);
+                setErrorLayoutVisibility(true);
+                break;
+        }
+    }
 
-	public void setProgressLayout(int layoutResId) {
-		mProgressLayoutContainer.removeAllViewsInLayout();
-		LayoutInflater.from(mContext).inflate(layoutResId, mProgressLayoutContainer);
-	}
+    // visibility
 
-	public void setProgressLayout(View layoutView) {
-		mProgressLayoutContainer.removeAllViewsInLayout();
-		mProgressLayoutContainer.addView(layoutView);
-	}
+    private void setProgressLayoutVisibility(boolean visible) {
+        if (mProgressLayoutContainer == null) {
+            if (visible) {
+                inflateProgressLayoutContainer();
+            }
+        } else {
+            mProgressLayoutContainer.setVisibility(visible ? View.VISIBLE : View.GONE);
+        }
+    }
 
-	public void setEmptyLayout(int layoutResId) {
-		mEmptyLayoutContainer.removeAllViewsInLayout();
-		LayoutInflater.from(mContext).inflate(layoutResId, mEmptyLayoutContainer);
-	}
+    private void setEmptyLayoutVisibility(boolean visible) {
+        if (mEmptyLayoutContainer == null) {
+            if (visible) {
+                inflateEmptyLayoutContainer();
+            }
+        } else {
+            mEmptyLayoutContainer.setVisibility(visible ? View.VISIBLE : View.GONE);
+        }
+    }
 
-	public void setEmptyLayout(View layoutView) {
-		mEmptyLayoutContainer.removeAllViewsInLayout();
-		mEmptyLayoutContainer.addView(layoutView);
-	}
-	
-	public void setErrorLayout(int layoutResId) {
-		mErrorLayoutContainer.removeAllViewsInLayout();
-		LayoutInflater.from(mContext).inflate(layoutResId, mErrorLayoutContainer);
-	}
-	
-	public void setErrorLayout(View layoutView) {
-		mErrorLayoutContainer.removeAllViewsInLayout();
-		mErrorLayoutContainer.addView(layoutView);
-	}
+    private void setContentLayoutVisibility(boolean visible) {
+        mContentViewContainer.setVisibility(visible ? View.VISIBLE : View.GONE);
+    }
+
+    private void setErrorLayoutVisibility(boolean visible) {
+        if (mErrorLayoutContainer == null) {
+            if (visible) {
+                inflateErrorLayoutContainer();
+            }
+        } else {
+            mErrorLayoutContainer.setVisibility(visible ? View.VISIBLE : View.GONE);
+        }
+    }
+
+    // inflate ViewStub
+
+    private void inflateProgressLayoutContainer() {
+        ViewStub viewStub = (ViewStub) findViewById(R.id.viewStub_progress);
+        viewStub.inflate();
+        mProgressLayoutContainer = (FrameLayout) findViewById(R.id.status_progress);
+        LayoutInflater.from(mContext).inflate(mProgressLayout, mProgressLayoutContainer);
+        mProgressLayoutContainer.setVisibility(View.VISIBLE);
+    }
+
+    private void inflateEmptyLayoutContainer() {
+        ViewStub viewStub = (ViewStub) findViewById(R.id.viewStub_empty);
+        viewStub.inflate();
+        mEmptyLayoutContainer = (FrameLayout) findViewById(R.id.status_empty);
+        LayoutInflater.from(mContext).inflate(mEmptyLayout, mEmptyLayoutContainer);
+        mEmptyLayoutContainer.setVisibility(View.VISIBLE);
+    }
+
+    private void inflateErrorLayoutContainer() {
+        ViewStub viewStub = (ViewStub) findViewById(R.id.viewStub_error);
+        viewStub.inflate();
+        mErrorLayoutContainer = (FrameLayout) findViewById(R.id.status_error);
+        LayoutInflater.from(mContext).inflate(mErrorLayout, mErrorLayoutContainer);
+        mErrorLayoutContainer.setVisibility(View.VISIBLE);
+    }
+
+    // set layout
+
+    public void setProgressLayout(int layoutResId) {
+        if (mProgressLayoutContainer == null) {
+            inflateProgressLayoutContainer();
+        }
+        mProgressLayoutContainer.removeAllViewsInLayout();
+        LayoutInflater.from(mContext).inflate(layoutResId, mProgressLayoutContainer);
+    }
+
+    public void setProgressLayout(View layoutView) {
+        if (mProgressLayoutContainer == null) {
+            inflateProgressLayoutContainer();
+        }
+        mProgressLayoutContainer.removeAllViewsInLayout();
+        mProgressLayoutContainer.addView(layoutView);
+    }
+
+    public void setEmptyLayout(int layoutResId) {
+        if (mEmptyLayoutContainer == null) {
+           inflateEmptyLayoutContainer();
+        }
+        mEmptyLayoutContainer.removeAllViewsInLayout();
+        LayoutInflater.from(mContext).inflate(layoutResId, mEmptyLayoutContainer);
+    }
+
+    public void setEmptyLayout(View layoutView) {
+        if (mEmptyLayoutContainer == null) {
+            inflateEmptyLayoutContainer();
+        }
+        mEmptyLayoutContainer.removeAllViewsInLayout();
+        mEmptyLayoutContainer.addView(layoutView);
+    }
+
+    public void setErrorLayout(int layoutResId) {
+        if (mErrorLayoutContainer == null) {
+            inflateErrorLayoutContainer();
+        }
+        mErrorLayoutContainer.removeAllViewsInLayout();
+        LayoutInflater.from(mContext).inflate(layoutResId, mErrorLayoutContainer);
+    }
+
+    public void setErrorLayout(View layoutView) {
+        if (mErrorLayoutContainer == null) {
+            inflateErrorLayoutContainer();
+        }
+        mErrorLayoutContainer.removeAllViewsInLayout();
+        mErrorLayoutContainer.addView(layoutView);
+    }
 }
